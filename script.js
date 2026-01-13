0981791b863d91696444e78861b9e93a92b74f70
Tanner Petreman <tanner.petreman@edu.sait.ca>
2026-01-12 22:47:27 -0700

    Created the js loop for listing the names in the console and listing them on the webpage under section with id=excersise 1, also fied bug where the javascript was loaded at the top of the page, moved it to the bottom of the body.

--- script.js
+++ script.js
@@ -17,7 +17,18 @@ const characters = [
 // broken test data for exercise 6
 
 // 1. Iterate through the characters array and output each character's name to the console using console.log(). Then, dynamically create <li> elements for each character name and append them to the HTML unordered list element with the id "names-list".
-
+const namesList = document.getElementById("names-list");
+// assigning i as 0, then counting how many names are in the array and making sure it doesnt go over the count then adds 1
+for (let i = 0; i < characters.length; i++) {
+  //print in console
+  const character = characters[i];
+  console.log(character.name);
+  
+//add li elements to the html page
+  const list = document.createElement("li");
+  list.textContent = character.name;
+  namesList.appendChild(list);
+}
 // 2. Filter the characters array to find only those characters whose age property is less than 40. Log each filtered character's name to the console. Then, dynamically create <li> elements for each filtered character and append them to the HTML unordered list element with the id "young-characters-list".
 
 // 3. Build a reusable function that accepts an array of character objects as a parameter. Inside the function, iterate through the array and extract each character's name property. Dynamically generate <li> elements for each name and append them to a target HTML list element. Call this function with the characters array and render the results in the unordered list with id "function-list".
